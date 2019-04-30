c766a44e31bc2a2916424d82bbeafcc167493fc8
jagarapusrinivasarao <jagarapusrinivas@gmail.com>
2019-04-30 18:16:28 +0530

    Auto-generated commit

--- app/models/current-weather.ts
+++
@@ -1,9 +0,0 @@
-export class CurrentWeather {
-  constructor( public cityName?: string,
-                public temp?: string,
-                public icon?: string,
-                public weatherKind?: string,
-                public tempMax?: string,
-                public tempMin?: string ) {}
-}
-
--- app/models/forecast.ts
+++
@@ -1,7 +0,0 @@
-export class Forecast {
-  constructor(public day: string,
-              public icon: string,
-              public temp: string,
-              public tempMax: string,
-              public tempMin: string ) {}
-}
